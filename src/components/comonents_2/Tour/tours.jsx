db467ba49f1f54abde77e4d39bb32c17803eaaf5
akyl <mirlanovakylbek192@gmail.com>
2024-03-01 07:07:19 +0300

    Nurchik

--- src/components/comonents_2/Tour/tours.jsx
+++ src/components/comonents_2/Tour/tours.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import './Tour.scss'
+import tour_1 from '../../img/tour_1.jpg'
 
 export default function Tourchik() {
     return (
         <div className="wrap">
             <div className="tour_container">
                 <div className="tour_text">
-                    <h1>vure fhfneiefry374rq fn fhwf387yr23rt2q387q y32rq28uyr298y8 26ry    2uirqh73wyru2t738g26q7ry2h7gq38urwfey8wrf27</h1>
-                    <h2>fedjwncrb272qx9nrqijedhbwhyduwegcbuifexbgfescfhasjcgfyhukawcjbfygsxhjfnysbehjfgsecyfhsgbczdgvhasjcfhevbjdsbfhjg</h2>
+                    <h1>Гарантированые туры</h1>
+                    <p>Присоединяйтесь к нашим гарантированным пешеходным турам и отправляйтесь в удивительные приключения по <br />
+                    самым красивым уголкам природы Кыргызстана. Наши туры специально разработаны для ценителей активного <br />
+                    отдыха и обещают незабываемые впечатления и взаимодействие с уникальной культурой и природой этой страны.</p>
+                </div>
+                <div className="tour_blogs">
+                    <div className="tour_img">
+                        <img src={tour_1} alt="" />
+                    </div>
+                    
                 </div>
             </div>
         </div>
